2c2960847420577a07d995c1fb04976073da125e
Ganesh Bhat <Ganeshbhat777bs@gmail.com>
2024-06-16 00:34:47 +0530

    First commit

--- client/src/Components/ShowVideo/ShowVideo.jsx
+++ client/src/Components/ShowVideo/ShowVideo.jsx
@@ -12,7 +12,7 @@ function ShowVideo({vid}) {
       <div className="vid">
         
           <div className='video'>
-            <video src={`https://youtubeclone5031.herokuapp.com/${vid.filePath}`}></video>
+            <video src={`https://youtubeclone4123.onrender.com/${vid.filePath}`}></video>
           </div>
         
         <div className="vid_desc">
